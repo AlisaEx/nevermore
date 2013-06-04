9878a22b9aa3c2461e9e094cbb41a6b9b586dc99
AlisaEx <eatliightning@gmail.com>
2013-06-04 15:30:05 -0400

    got simon closer to the edge of the earth

--- game.js
+++ game.js
@@ -1,19 +1,18 @@
 require(['frozen/GameCore', 'frozen/ResourceManager', 'dojo/keys'], function(GameCore, ResourceManager, keys){
-
-  // var simonx = (rimx.height/2-simon.height);
-  // var simony = -130;
-  var rimx = 260;
-  var rimy = 200;
+  var earthx = 260;
+  var earthy = 200;
   var simonSpeed = 2.5;
-  var angularVelocity = .03;
+  var angularVelocity = .05;
   var angle = 0;
 
 
   var rm = new ResourceManager();
   var backImg = rm.loadImage('images/background.png');
-  var rim = rm.loadImage('images/outterRim.png');
+  var earth = rm.loadImage('images/outterRim.png');
   var simon = rm.loadImage('images/simon.png');
   
+  var simonx = (-(earthx-(earth.width/2)));
+  var simony = (-(earthy-(earth.height/2)));
 
   var game = new GameCore({
     canvasId: 'canvas',
@@ -31,7 +30,7 @@ require(['frozen/GameCore', 'frozen/ResourceManager', 'dojo/keys'], function(Gam
 
       if(im.keyActions[keys.RIGHT_ARROW].isPressed()){
         angle += angularVelocity;
-        console.log(simon.x);
+        console.log(-(earthx-(earth.width/2)));
       }
 
     },
@@ -39,12 +38,12 @@ require(['frozen/GameCore', 'frozen/ResourceManager', 'dojo/keys'], function(Gam
     },
     draw: function(context){
       context.drawImage(backImg, 0, 0, this.width, this.height);
-      context.drawImage(rim, rimx, rimy);
+      context.drawImage(earth, earthx, earthy);
       context.save();
       context.translate(backImg.width/2, backImg.height/2);
       context.rotate(angle);
-      context.drawImage(simon, -(rim.height/2-simon.height), -(rim.width/2-simon.width));
-      context.translate(-(rim.width/2), -(rim.height/2));
+      context.drawImage(simon,((earthx/2)-(earth.width/2)),((earthy/2)-(earth.height/2)));
+      context.translate(-(backImg.width/2), -(backImg.height/2));
       context.restore();
     }
   });
