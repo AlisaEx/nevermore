f8eb781ed3d7095f9f08fc2c03b0edd1d11d54d2
AlisaEx <eatliightning@gmail.com>
2013-06-04 14:11:06 -0400

    messed around with paper.js, added circular background

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 require(['frozen/GameCore', 'frozen/ResourceManager', 'dojo/keys'], function(GameCore, ResourceManager, keys){
 
-  var simonx = 400;
-  var simony = 193;
+  // var simonx = (rimx.height/2-simon.height);
+  // var simony = -130;
   var rimx = 260;
   var rimy = 200;
   var simonSpeed = 2.5;
@@ -13,8 +13,6 @@ require(['frozen/GameCore', 'frozen/ResourceManager', 'dojo/keys'], function(Gam
   var backImg = rm.loadImage('images/background.png');
   var rim = rm.loadImage('images/outterRim.png');
   var simon = rm.loadImage('images/simon.png');
-  rim.x = 442;
-  rim.y = 382;
   
 
   var game = new GameCore({
@@ -33,23 +31,20 @@ require(['frozen/GameCore', 'frozen/ResourceManager', 'dojo/keys'], function(Gam
 
       if(im.keyActions[keys.RIGHT_ARROW].isPressed()){
         angle += angularVelocity;
+        console.log(simon.x);
       }
 
-      // if(im.keyActions[keys.SPACE].isPressed()){
-      //   simony-= simonSpeed;
-      // }
     },
     update: function(millis){
-      
     },
     draw: function(context){
       context.drawImage(backImg, 0, 0, this.width, this.height);
-      context.drawImage(rim, 260, 200);
+      context.drawImage(rim, rimx, rimy);
       context.save();
       context.translate(backImg.width/2, backImg.height/2);
       context.rotate(angle);
-      context.drawImage(simon, 0, -150);
-      context.translate(-(backImg.width/2), -(backImg.height/2));
+      context.drawImage(simon, -(rim.height/2-simon.height), -(rim.width/2-simon.width));
+      context.translate(-(rim.width/2), -(rim.height/2));
       context.restore();
     }
   });
