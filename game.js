3bcfee69d1c65f587c2c874e0b02b683d41095e9
AlisaEx <eatliightning@gmail.com>
2013-06-14 15:44:17 -0400

    added sound for gunther

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-require(['frozen/GameCore', 'frozen/ResourceManager', 'frozen/Sprite', 'frozen/Animation', 'utils/degreesToRadians.js','plugins/loadImage!images/BMO.png', 'plugins/loadImage!images/ChainsawBear.png', 'plugins/loadImage!images/Gunter.png', 'plugins/loadImage!images/KnifeBunny.png', 'dojo/keys'],
-  function(GameCore, ResourceManager, Sprite, Animation, degreesToRadians, bmoImg, bearImg, gunterImg, bunnyImg, keys){
+require(['frozen/GameCore', 'frozen/ResourceManager', 'frozen/Sprite', 'frozen/Animation', 'utils/degreesToRadians.js','plugins/loadImage!images/BMO.png', 'plugins/loadImage!images/ChainsawBear.png', 'plugins/loadImage!images/Gunther.png', 'plugins/loadImage!images/KnifeBunny.png', 'plugins/loadSound!/sounds/guntherNoise.mp3', 'dojo/keys'],
+  function(GameCore, ResourceManager, Sprite, Animation, degreesToRadians, bmoImg, bearImg, guntherImg, bunnyImg, guntherSnd, keys){
   'use strict';
 
       /// global variables ///
@@ -7,6 +7,7 @@ require(['frozen/GameCore', 'frozen/ResourceManager', 'frozen/Sprite', 'frozen/A
   var updateables     = [];
   var rotateables     = [];
   var collidables     = [];
+  var playCnt         = 0;
 
       /// declare entities ///
   var simon  = {
@@ -22,13 +23,13 @@ require(['frozen/GameCore', 'frozen/ResourceManager', 'frozen/Sprite', 'frozen/A
       context.restore();      
     }
   };
-  var gunter = {
+  var gunther = {
     sprite: new Sprite({x: 640, y: 300, width: 100, height: 32, dx: 0, dy: 0, collisionRadius: 56}),
     angle: 90,
     doesCollide: function (context){
       context.font = 'italic 12pt Calibri';
       context.fillStyle = 'black';
-      context.fillText('Meep.', gunter.sprite.x+10, gunter.sprite.y);}
+      context.fillText('Meep.', gunther.sprite.x+10, gunther.sprite.y);}
   };
   var bunny  = {
     sprite: new Sprite({x: 460, y: 540, width: 896, height: 40, dx: 0, dy: 0, collisionRadius: 56}),
@@ -47,27 +48,22 @@ require(['frozen/GameCore', 'frozen/ResourceManager', 'frozen/Sprite', 'frozen/A
     context.fillText("Why do I keep exploding?", bear.sprite.x-10, bear.sprite.y);}
   };
       /// push objects to their respective arrays ///
-  collidables.push(gunter);collidables.push(bunny);collidables.push(bear);
-  updateables.push(simon);updateables.push(gunter);updateables.push(bunny);updateables.push(bear);
-  rotateables.push(gunter);rotateables.push(bunny);rotateables.push(bear);
+  collidables.push(gunther);collidables.push(bunny);collidables.push(bear);
+  updateables.push(simon);updateables.push(gunther);updateables.push(bunny);updateables.push(bear);
+  rotateables.push(gunther);rotateables.push(bunny);rotateables.push(bear);
 
       /// Resource Manager ///
   var rm = new ResourceManager({
-    soundsDir: 'sounds/',
     imageDir: 'images/'
   });
       /// Load Images ///
   var backImg = rm.loadImage('background.png');
   var earth = rm.loadImage('outterRim.png');
 
-      /// Load Sounds ///
-  var music = rm.loadSound('music.ogg');
-  var musicSource = null;
-
       /// set the sprite animation to use 8 frames, 100 millis/frame, spritesheet, 96x96 pixels
   simon.sprite.anim = Animation.prototype.createFromSheet(10,100,bmoImg, 32,40)
   bear.sprite.anim = Animation.prototype.createFromSheet(71, 100, bearImg, 120, 128);
-  gunter.sprite.anim = Animation.prototype.createFromSheet(37, 100, gunterImg, 56, 32);
+  gunther.sprite.anim = Animation.prototype.createFromSheet(37, 100, guntherImg, 56, 32);
   bunny.sprite.anim = Animation.prototype.createFromSheet(16, 100, bunnyImg, 56, 40);
 
       /// updates the real-time position of the main character ///
@@ -119,11 +115,14 @@ require(['frozen/GameCore', 'frozen/ResourceManager', 'frozen/Sprite', 'frozen/A
     },
         /// updates the game state every millisecond ///
     update: function(millis){
-      // musicSource = rm.playSound(music, true); //Loop music.
       getPosition(simon.translatedPosition, simon.angle);
       updateables.forEach(function(entity){
         entity.sprite.update(millis);
       })
+      if (collides(simon.translatedPosition, gunther.sprite)===true && playCnt < 1){
+        guntherSnd.play();
+        playCnt = 1;
+      }
     },
     draw: function(context){
       if (gameStart === false){
